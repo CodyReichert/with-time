f5955cfd40bf8e816157b9a5b31ec74d7d83b9a3
Cody Reichert <cody@reichertbrothers.com>
2015-10-07 16:57:27 -0500

    Fix problem where this library works the first time but not the second
    I'm still not 100% sure why this works - but it does(?).

--- src/with-time.lisp
+++ src/with-time.lisp
@@ -1,7 +1,8 @@
 (in-package :cl-user)
 (defpackage with-time 
   (:use :cl)
-  (:export :with-time))
+  (:export :with-time
+           :*timer-stream*))
 (in-package :with-time)
 
 ;; Example:
@@ -12,7 +13,7 @@
 
 ;; Code:
 
-(defvar *timer-stream* (make-string-output-stream)
+(defparameter *timer-stream* (make-string-output-stream)
   "Redirect `*trace-output*' to here. Temporarily holds the previous
 time output.")
 
@@ -27,12 +28,25 @@ string."
   (clean-strings
    (cl-ppcre:split "([ ]{2,})" (get-output-stream-string stream))))
 
+(defun redirect-trace-output ()
+  "A helper function to capture `*trace-output*'."
+  (setf *trace-output* *timer-stream*))
+
+(defun reset-streams-and-redirect ()
+  "Set `*timer-stream* to a new, empty stream, and redirect `*trace-ouput*'
+to the new stream."
+  ;; For some reason we have to reset the *timer-stream* after it has been
+  ;; Read. So do that, then redirect *trace-output*. Is there a better way?
+  (setf *timer-stream* (make-string-output-stream))
+  (setf *trace-output* *timer-stream*))
+
 (defmacro with-time (&body body)
   "Wrap a function in this macro. It returns a values list containing
 first, the return value of the provided function, and second, the execution
 time of the function provided. The execution time is consed into a list
 to make it easier to programatically work with."
-  (setf *trace-output* *timer-stream*)
+  (redirect-trace-output)
   `(let ((fn-val (time ,@body))
           (times (cons-time-output `,*timer-stream*)))
+     (reset-streams-and-redirect)
      (values fn-val times)))
