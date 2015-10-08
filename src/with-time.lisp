5386b6d1ffe2a070d49be7e83c438dab625c0430
Cody Reichert <cody@reichertbrothers.com>
2015-10-07 20:32:13 -0500

    OK - implement our own (time) to alleviate stream issues

--- src/with-time.lisp
+++ src/with-time.lisp
@@ -28,25 +28,43 @@ string."
   (clean-strings
    (cl-ppcre:split "([ ]{2,})" (get-output-stream-string stream))))
 
-(defun redirect-trace-output ()
-  "A helper function to capture `*trace-output*'."
-  (setf *trace-output* *timer-stream*))
 
-(defun reset-streams-and-redirect ()
-  "Set `*timer-stream* to a new, empty stream, and redirect `*trace-ouput*'
-to the new stream."
-  ;; For some reason we have to reset the *timer-stream* after it has been
-  ;; Read. So do that, then redirect *trace-output*. Is there a better way?
-  (setf *timer-stream* (make-string-output-stream))
-  (setf *trace-output* *timer-stream*))
+(defun stream-time (&key real-time-ms user-run-time-us system-run-time-us
+                   gc-run-time-ms processor-cycles eval-calls
+                   lambdas-converted page-faults bytes-consed
+                   aborted)
+  (let ((total-run-time-us (+ user-run-time-us system-run-time-us)))
+    (format *timer-stream*
+            "~&Evaluation took:~%~
+                         ~@<  ~@;~/sb-impl::format-milliseconds/ of real time~%~
+                                 ~/sb-impl::format-microseconds/ of total run time ~
+                                  (~@/sb-impl::format-microseconds/ user, ~@/sb-impl::format-microseconds/ system)~%~
+                                 ~[[ Run times consist of ~/sb-impl::format-milliseconds/ GC time, ~
+                                                      and ~/sb-impl::format-milliseconds/ non-GC time. ]~%~;~2*~]~
+                                 ~,2F% CPU~%~
+                                 ~@[~:D form~:P interpreted~%~]~
+                                 ~@[~:D lambda~:P converted~%~]~
+                                 ~@[~:D processor cycles~%~]~
+                                 ~@[~:D page fault~:P~%~]~
+                                 ~:D bytes consed~%~
+                                 ~@[~%before it was aborted by a non-local transfer of control.~%~]~:>~%"
+            real-time-ms
+            total-run-time-us
+            user-run-time-us
+            system-run-time-us
+            (if (zerop gc-run-time-ms) 1 0)
+            gc-run-time-ms ;; Round up so we don't mislead by saying 0.0 seconds of non-GC time...
+            (- (ceiling total-run-time-us 1000) gc-run-time-ms)
+            (if (zerop real-time-ms) 100.0
+                (float (* 100 (/ (round total-run-time-us 1000) real-time-ms))))
+            eval-calls
+            lambdas-converted
+            processor-cycles
+            page-faults bytes-consed
+            aborted)))
 
-(defmacro with-time (&body body)
-  "Wrap a function in this macro. It returns a values list containing
-first, the return value of the provided function, and second, the execution
-time of the function provided. The execution time is consed into a list
-to make it easier to programatically work with."
-  (redirect-trace-output)
-  `(let ((fn-val (time ,@body))
-          (times (cons-time-output `,*timer-stream*)))
-     (reset-streams-and-redirect)
-     (values fn-val times)))
+
+(defmacro with-time (form)
+  `(let ((fn
+          (sb-impl::call-with-timing #'stream-time (lambda () ,form))))
+     (values fn (cons-time-output *timer-stream*))))
